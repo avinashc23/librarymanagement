d8dad937fb181d8c172744444a6a11d8c12bce9e
avinash <avinash230202@gmail.com>
2021-05-26 15:49:46 +0530

    try1

--- app.js
+++ app.js
@@ -151,12 +151,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port=process.env.PORT;
-if(port== null || port==""){
-  port=3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
 }
 
-
 app.listen(port, function() {
-  console.log("Server started successfully on port ");
+  console.log("Server started succesfully");
 });
